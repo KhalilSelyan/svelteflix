95b5cc0d37d84596923fa78bcf0dea52069fcb3d
KhalilSelyan <khalil@leodrive.ai>
2024-07-04 15:54:15 +0300

    Correct recommendation name in movie details

--- src/routes/movies/[id]/+page.server.ts
+++ src/routes/movies/[id]/+page.server.ts
@@ -6,6 +6,15 @@ export const load = async ({ params, fetch }) => {
 		append_to_response: 'images,videos,recommendations'
 	})) as MovieDetails;
 
+	// correct the recommendation name
+	// it comes out as "recommendations?6eaba0f5946bb7cedb6601f52d2158ca" instead of "recommendations"
+
+	// @ts-expect-error - this is a hack to fix the recommendation name
+	if (movie['recommendations?6eaba0f5946bb7cedb6601f52d2158ca']) {
+		// @ts-expect-error - this is a hack to fix the recommendation name
+		movie.recommendations = movie['recommendations?6eaba0f5946bb7cedb6601f52d2158ca'];
+	}
+
 	const trailer = movie.videos.results.find((video) => {
 		return video.official && video.site === 'YouTube' && video.type === 'Trailer';
 	});
